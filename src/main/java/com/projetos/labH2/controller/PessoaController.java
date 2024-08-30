ee824599fd52faaec0b25b0c36f705f782c79a95
sergioajiki <sergioajk@hotmail.com>
2024-08-30 16:13:51 -0400

    feat<PessoaController>: getAllPessoas

--- src/main/java/com/projetos/labH2/controller/PessoaController.java
+++ src/main/java/com/projetos/labH2/controller/PessoaController.java
@@ -7,13 +7,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class PessoaController {
     @Autowired
     private PessoaService pessoaService;
 
+    @GetMapping("/pessoas")
+    public List<PessoaVo> getAllPessoas() {
+        return pessoaService.getAllPessoas();
+    }
+
     @GetMapping("/pessoa/{id}")
     public PessoaVo getPessoaById(@PathVariable int id) {
         return pessoaService.getPessoaById(id);
     }
+
+
+
+
 }
--- src/main/java/com/projetos/labH2/service/PessoaService.java
+++ src/main/java/com/projetos/labH2/service/PessoaService.java
@@ -5,10 +5,16 @@ import com.projetos.labH2.labVO.PessoaVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class PessoaService {
     @Autowired
     private PessoaDao pessoaDao;
+
+    public List<PessoaVo> getAllPessoas() {
+        return pessoaDao.getAllPessoas();
+    }
 
     public PessoaVo getPessoaById(Integer id) {
         return pessoaDao.getPessoaById(id);
