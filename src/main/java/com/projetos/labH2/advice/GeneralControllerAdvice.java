299b680d9a86e84b1aceb7e0df87892ad169cd02
sergioajiki <sergioajk@hotmail.com>
2024-09-02 14:37:15 -0400

    feat: GeneralControllerAdvice

---
+++ src/main/java/com/projetos/labH2/advice/GeneralControllerAdvice.java
@@ -0,0 +1,30 @@
+package com.projetos.labH2.advice;
+
+import com.projetos.labH2.advice.exceptions.NotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GeneralControllerAdvice {
+    private final MessageSource messageSource;
+
+    @Autowired
+    public GeneralControllerAdvice(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<Problem> handleNotFoundException(NotFoundException exception) {
+        Problem problem = new Problem(
+                HttpStatus.BAD_REQUEST.value(),
+                "Informação não encontrada",
+                exception.getMessage(),
+                null
+        );
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(problem);
+    }
+}
