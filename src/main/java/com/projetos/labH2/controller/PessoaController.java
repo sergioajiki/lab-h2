e53295abd3e4714dfd32060acd53baa231b7b283
sergioajiki <sergioajk@hotmail.com>
2024-09-02 07:52:45 -0400

    build: adicionado dependencia sprigdoc para configurar o swagger

--- src/main/java/com/projetos/labH2/controller/PessoaController.java
+++ src/main/java/com/projetos/labH2/controller/PessoaController.java
@@ -2,17 +2,20 @@ package com.projetos.labH2.controller;
 
 import com.projetos.labH2.labVO.PessoaVo;
 import com.projetos.labH2.service.PessoaService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@Tag(name = "Pessoas" )
+@RequestMapping("/pessoa")
 public class PessoaController {
     @Autowired
     private PessoaService pessoaService;
 
-    @GetMapping("/pessoas")
+    @GetMapping
     public List<PessoaVo> getAllPessoas() {
         return pessoaService.getAllPessoas();
     }
