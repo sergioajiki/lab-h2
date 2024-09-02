937adbf6329071b6310d568e33efd502eccf3c62
sergioajiki <sergioajk@hotmail.com>
2024-09-02 08:01:30 -0400

    refactor: adicionando informações para documentação da api pelo swagger

--- src/main/java/com/projetos/labH2/controller/PessoaController.java
+++ src/main/java/com/projetos/labH2/controller/PessoaController.java
@@ -2,6 +2,7 @@ package com.projetos.labH2.controller;
 
 import com.projetos.labH2.labVO.PessoaVo;
 import com.projetos.labH2.service.PessoaService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -9,23 +10,29 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@Tag(name = "Pessoas" )
+@Tag(name = "Pessoas")
 @RequestMapping("/pessoa")
 public class PessoaController {
     @Autowired
     private PessoaService pessoaService;
 
+    // Endpoint exibir uma lista com todas as pessoas
     @GetMapping
+    @Operation(description = "Lista todas as pessoas")
     public List<PessoaVo> getAllPessoas() {
         return pessoaService.getAllPessoas();
     }
 
-    @GetMapping("/pessoa/{id}")
+    // Endpoint para buscar uma pessoa pelo ID
+    @GetMapping("/{id}")
+    @Operation(description = "Busca uma pessoa selecionada por id")
     public PessoaVo getPessoaById(@PathVariable int id) {
         return pessoaService.getPessoaById(id);
     }
 
+    // Endpoint para cadastrar uma pessoa
     @PostMapping("/cadastrar")
+    @Operation(description = "Cadastra uma pessoa")
     public String cadastrarPessoa(@RequestBody PessoaVo pessoa) {
         try {
             pessoaService.cadastrarPessoa(pessoa);
@@ -35,9 +42,10 @@ public class PessoaController {
         }
     }
 
-    // Endpoint para deletar uma pessoa pelo ID
-    @GetMapping("/apagar/{id}")
+    // Endpoint para apagar uma pessoa pelo ID
+    @DeleteMapping("/{id}")
+    @Operation(description = "Apaga o registro da pessoa selecionada por id")
     public void deletarPessoa(@PathVariable int id) {
-         pessoaService.deletarPessoaById(id);
+        pessoaService.deletarPessoaById(id);
     }
 }
