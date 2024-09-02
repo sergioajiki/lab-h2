643c180c37a92912dd30f1d20775e4fbbd05c481
sergioajiki <sergioajk@hotmail.com>
2024-09-02 17:30:51 -0400

    feat: MethodArgumentNotValidException in GeneralControllerAdvice

--- src/main/java/com/projetos/labH2/advice/GeneralControllerAdvice.java
+++ src/main/java/com/projetos/labH2/advice/GeneralControllerAdvice.java
@@ -3,10 +3,15 @@ package com.projetos.labH2.advice;
 import com.projetos.labH2.advice.exceptions.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @ControllerAdvice
 public class GeneralControllerAdvice {
@@ -26,5 +31,27 @@ public class GeneralControllerAdvice {
                 null
         );
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(problem);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Problem> handleFieldNotFound(MethodArgumentNotValidException exception) {
+        List<ErrorMessageRecord> problemList = new ArrayList<>();
+
+        exception.getBindingResult().getFieldErrors().forEach(e -> {
+            String detail = messageSource.getMessage(e, LocaleContextHolder.getLocale());
+            ErrorMessageRecord messageDetail = new ErrorMessageRecord(
+                    e.getField(),
+                    detail
+            );
+            problemList.add(messageDetail);
+        });
+
+        Problem problem = new Problem(
+                HttpStatus.BAD_REQUEST.value(),
+                "Invalid Parameters",
+                "Invalid Request Body",
+                problemList
+        );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problem);
     }
 }
