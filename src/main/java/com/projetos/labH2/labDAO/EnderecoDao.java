f8a6f61d2eaa1e7486d2a9ac0ecf1f5dcb5adcc8
sergioajiki <sergioajk@hotmail.com>
2024-09-04 10:02:17 -0400

    feat: EnderecoVo interface com crud básico

---
+++ src/main/java/com/projetos/labH2/labDAO/EnderecoDao.java
@@ -0,0 +1,20 @@
+package com.projetos.labH2.labDAO;
+
+import com.projetos.labH2.labVO.EnderecoVo;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface EnderecoDao {
+    void insertEndereco(EnderecoVo endereco);
+
+    List<EnderecoVo> getAllEndereco();
+
+    EnderecoVo getEnderecoById(@Param("id") Long id);
+
+    void updateEnderecoById(@Param("id") Long id, @Param("endereco") EnderecoVo endereco);
+
+    void deleteEnderecoById(@Param("id") Long id);
+}
