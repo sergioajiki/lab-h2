e22f3c33e01a9f1136c0e3d3c246fa6b9cf63e61
sergioajiki <sergioajk@hotmail.com>
2024-09-04 09:24:04 -0400

    feat: adicionado tabela endereco em schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,7 +4,19 @@ CREATE SCHEMA IF NOT EXISTS h2dev;
 -- Usando o esquema criado
 SET SCHEMA h2dev;
 
--- Criação da Tabela
+-- Criação da tabela de endereços
+CREATE TABLE IF NOT EXISTS endereco (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    logradouro VARCHAR(255) NOT NULL,
+    numero VARCHAR(255),
+    complemento VARCHAR(255),
+    bairro VARCHAR(255),
+    cep VARCHAR(255) NOT NULL,
+    cidade VARCHAR(255 NOT NULL),
+    estado VARCHAR(2) NOT NULL -- Estado em formato de sigla
+);
+
+-- Criação da tabela de pessoas
 CREATE TABLE IF NOT EXISTS pessoa (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
