8bd9c8c4ce2644eac0bd20e56c9f43608978b4c4
sergioajiki <sergioajk@hotmail.com>
2024-09-05 14:10:12 -0400

    feat: Adicionar RequestPessoaVo para melhorar a clareza e a organização do cadastro de pessoas

--- src/main/java/com/projetos/labH2/controller/PessoaController.java
+++ src/main/java/com/projetos/labH2/controller/PessoaController.java
@@ -7,7 +7,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
---
+++ src/main/java/com/projetos/labH2/labVO/RequestPessoaVo.java
@@ -0,0 +1,24 @@
+package com.projetos.labH2.labVO;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class RequestPessoaVo {
+
+    @NotEmpty(message = "O campo nome é obrigatório")
+    private String nome;
+
+    @NotNull(message = "O campo idade é obrigatório")
+    private Integer idade;
+
+    @NotEmpty(message = "O campo email é obrigatório")
+    private String email;
+
+    @NotNull(message = "O campo data_nascimento é obrigatório")
+    private String data_nascimento;
+
+    // ID do endereço é opcional, pode ser null
+    private Long enderecoId;
+}
